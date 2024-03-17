75ccdaa7563a1b093517a487e2a88939e48b2297
Omer Yassir <omerym10@gmail.com>
2024-03-17 23:52:12 +0200

    adsfdsgdsg

---
+++ js/bookLoader.js
@@ -0,0 +1,14 @@
+﻿
+function refresh() {
+    document.getElementById("searchBar").value = "";
+    var container = document.getElementById("books");
+    container.innerHTML = null;
+    for (var i = 1; i < 10; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = `ID: ${i}<br />Name: book${i}<br />Author: author${i}<br />Description:<br />This book is called book${i} and is written by author${i}.<br /><br />`;
+        container.appendChild(div);
+    }
+}
+function search() {
+
+}
