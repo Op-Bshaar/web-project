99c0662d78d876a96f39e06a2d3c27ce29be5524
Omer Yassir <omerym10@gmail.com>
2024-04-21 01:25:07 +0200

    F+

--- js/bookLoader.js
+++ js/bookLoader.js
@@ -1,21 +1,62 @@
 ﻿
+class book {
+    constructor(id, name, author,catogery=' ', description =' ') {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.catogery = catogery;
+        this.description = description;
+    }
+}
 function refresh() {
     document.getElementById("searchBar").value = "";
     var container = document.getElementById("books");
     container.innerHTML = '';
-    for (var i = 1; i <= 10; i++) {
+    for (book of getBooks()) {
         var span = document.createElement("span");
-        span.innerHTML = `<div onclick ="window.location.href='BookDetails.html?id=${i}'" class="book">Name: Book${i}<br />Author: Author${i}<br /></div >`
+        span.innerHTML = `<div onclick ="window.location.href='BookDetails.html?id=${book.id}'" class="book">Name: ${book.name}<br />Author: ${book.author}<br /></div >`
         container.appendChild(span);
     }
+}
+function* getBooks()
+{
+    books = JSON.parse(localStorage.getItem('books'));
+    if (books == null || books.length == 0) {
+
+        books = [new book(1, 'the book', 'omer'), new book(2, 'xyz', 'ali')];
+        localStorage.setItem('books', JSON.stringify(books));
+        alert('created 2 test books');
+    }
+    for (book of books) {
+        yield book;
+    }
+}
+function addBook() {
+    books = JSON.parse(localStorage.getItem('books'));
+    n = document.getElementsByName('Name')[0].value;
+    id = document.getElementsByName('Id')[0].value;
+    author = document.getElementsByName('Author')[0].value;
+    category = document.getElementsByName('Category')[0].value;
+    description = document.getElementsByName('Description')[0].value;
+    books.push(new book(id, n, author, category, description));
+    localStorage.setItem('books', JSON.stringify(books));
 }
 function loadBook() {
     const params = new URLSearchParams(document.location.search);
     const id = params.get("id");
-    document.getElementById("title").innerHTML = `Book${id}`;
-    document.getElementById("author").innerHTML = `author${id}`;
-    document.getElementById("catogery").innerHTML = `catogery${id}`;
-    document.getElementById("description").innerHTML = `This book is called Book${id}, and is written by author${id}. It is part of catogery${id} catogery.`;
+    books = JSON.parse(localStorage.getItem('books'));
+    if (books == null ) {
+        return;
+    }
+    for (b of books) {
+        if (b.id == id) {
+            document.getElementById("title").innerHTML = b.name;
+            document.getElementById("author").innerHTML = b.author;
+            document.getElementById("catogery").innerHTML = b.catogery;
+            document.getElementById("description").innerHTML =b.description;
+            break;
+        }
+    }
 }
 function search() {
 
