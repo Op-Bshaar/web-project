d852e183ff200069d12199d3545724f27e1d1021
Ayman-zain <aymnzyn85@gmail.com>
2024-05-04 19:01:04 +0300

    cherry pick

--- Library/catalog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.catalog, name='catalog'),
-    path('about/', views.about, name='About'),
-    path('contact/', views.contact, name='ContactUs'),
-
-]
